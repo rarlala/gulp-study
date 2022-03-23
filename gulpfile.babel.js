f28f60afe30ecbf16630209019f1ef99045f44b9
rarlala <wtw9611@gmail.com>
2022-03-24 08:56:10 +0900

    ✨ [Feat] Scss compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,10 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
   pug: {
@@ -13,6 +17,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
   }
 }
 
@@ -27,14 +36,21 @@ const webserver = () => gulp.src("build")
 
 const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest))
 
+const styles = () => gulp.src(routes.scss.src)
+  .pipe(sass().on("error", sass.logError))
+  .pipe(autoprefixer())
+  .pipe(miniCSS())
+  .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.src, styles);
 }
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
